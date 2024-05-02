f2ab062746bd76f9dbb27e8a6f426fd5ce260bf0
Staz212 <168725406+Staz212@users.noreply.github.com>
2024-05-02 18:31:16 +0700

    ban.js

---
+++ commands/moderation/ban.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Ban a user from the server.')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('Select the user to ban')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Specify the reason for banning the user')
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason') || 'No reason specified';
+    await interaction.reply(`Banned user ${user} for: ${reason}`);
+  },
+};
+
+/*
+basic code ban commands
+by staz
+*/
