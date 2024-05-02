916f98b6ce7f0497cc4ab0e37500c7fe85ca939f
Staz212 <168725406+Staz212@users.noreply.github.com>
2024-05-02 18:34:35 +0700

    serverinfo.js

---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('serverinfo')
+		.setDescription('Provides information about the server.'),
+	async execute(interaction) {
+		// interaction.guild adalah objek yang mewakili Persekutuan tempat perintah dijalankan
+		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+	},
+};
+
+/*
+basic kode serverinfo
+by staz
+*/
